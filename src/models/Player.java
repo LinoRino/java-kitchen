23f745653c2d0378ad30734b7cf4c24eb75c09ba
LinoRino <omurakenshin@gmail.com>
2023-12-02 00:08:21 +0900

    Change to record

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,16 +1,9 @@
 package models;
 
-public class Player {
-    public final String name;
-    public final int id;
-    public Player(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
+public record Player(String name) {
 
     public void eat(Eatable eatable) {
         eatable.eat(this);
-        System.out.printf("%s ate %s\n", this.name, eatable.getName());
+        System.out.printf("%s ate %s\n", this.name, eatable.getClass().getSimpleName());
     }
-
 }
