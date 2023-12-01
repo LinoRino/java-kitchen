abdbd3df5ac87d4c474ad06567be2b33fd0910de
LinoRino <omurakenshin@gmail.com>
2023-12-02 00:12:13 +0900

    Revert "Change to record"
    This reverts commit 23f745653c2d0378ad30734b7cf4c24eb75c09ba.

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,9 +1,16 @@
 package models;
 
-public record Player(String name) {
+public class Player {
+    public final String name;
+    public final int id;
+    public Player(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public void eat(Eatable eatable) {
         eatable.eat(this);
-        System.out.printf("%s ate %s\n", this.name, eatable.getClass().getSimpleName());
+        System.out.printf("%s ate %s\n", this.name, eatable.getName());
     }
+
 }
